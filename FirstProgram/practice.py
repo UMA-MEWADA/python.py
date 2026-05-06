033c3e22b1a70f549b63f94015d1829adfa9440b
UMA-MEWADA <umamewada120@gmail.com>
2026-05-06 22:35:50 +0000

    practice set

---
+++ FirstProgram/practice.py
@@ -0,0 +1,14 @@
+first = int(input("enter first :"))
+second = int(input("enter second :"))
+
+print("sum =", first + second)
+
+#square area
+print("Square area")
+side = float(input("enter square side : "))
+print("area =", side**2)
+
+# Average float number
+A = float(input("enter first : "))
+B = float(input("enter Second : "))
+print ("avg =",(A+B)/2 )
