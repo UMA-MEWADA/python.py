a0af94adeba1728d4fe8f8e3b1263222c119f345
UMA-MEWADA <umamewada120@gmail.com>
2026-05-06 11:50:36 +0000

    basic python

---
+++ FirstProgram/firstProgram.py
@@ -0,0 +1,54 @@
+print("Hello world")
+print("I am uma mewada")
+print("this is basic","program python")
+print(23) #intiger
+print("23") #string
+name = "shivani"
+age = 20 ;
+age2 = age #assine operater 
+price = 25.99
+print("my name is :", name)
+print("my age is :", age)
+print(age2)
+print(type(name))
+print(type(age))
+print(type(price))
+#string data three way initialize
+name1 ="sk"
+name2 = 'sk'
+name3 = '''sk'''
+print(name1)
+print(name2)
+print(name3)
+
+#boolean data type
+age1 = 26
+old = False
+a = None
+print(type(old))
+print(type(a))
+#Arithmetic operaters( + , _ , * , /)
+A = 5
+B = 7
+sum = A + B
+print(sum)
+# types of token 
+# 1 punctuators = punctuators are symmbols to organize structure in program Example () , {} ,[],@ , # , -= ,+= ,/= etc
+C , D = 2 ,3
+E = 4
+print(C+D*E)
+print(C/D) # resul of two integer will be float
+F,G = 10 ,5.0 
+H = F*G #float result
+print(H)
+M,N = 1.5 , 3
+R= M//N
+ #integer division with float result float CLOSEST integer  and round of small value example 1.5//3 = 0.5 but ans 0.0 example -12//5 =-2.4 but resullt = -3
+print(R, M/N)
+k , l = -5 ,2 
+r = k%l
+print(r) #output 1 it is give remender
+P, O = 5 , 2
+print(P%O) # OUTPUT 1
+S,T = 5,-2 
+print(S%T) #OUTPUT -1 
