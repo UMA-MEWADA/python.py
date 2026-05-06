df5560c6d6d8b47c71fec80dae2f11fd034c50f8
UMA-MEWADA <umamewada120@gmail.com>
2026-05-06 22:18:46 +0000

    type coveersion

---
+++ FirstProgram/typeConversion.py
@@ -0,0 +1,16 @@
+#type conversion
+a = float("2")
+b = 4.25
+
+print(type(a))
+A = 32.2
+A = str(A)
+print(type(A))
+ 
+name = input("enter name :")
+age = int(input("enter age: "))
+marks = float(input("enter marks : "))
+
+print("welcome", name)
+print("age =", age)
+print("marks =", marks)
